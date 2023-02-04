38e9665fbffd5633e990f22e83632252df203c7a
Joao-Arthur <Joao-Arthur@users.noreply.github.com>
2023-02-04 19:12:02 +0000

    deploy: 154a4548b296c8a412f104db432c73d6f93f2def

---
+++ _next/static/xlfA2l9kLoB4aBmDuQ_7D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-efad8e7075d8f5d7.js"],"/_error":["static/chunks/pages/_error-99cb4b575ea0906f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
