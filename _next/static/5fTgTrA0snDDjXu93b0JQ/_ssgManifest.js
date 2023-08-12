83c40f9ca21277be3db10f0d6a495fd8e47c145c
Joao-Arthur <Joao-Arthur@users.noreply.github.com>
2023-08-12 03:01:34 +0000

    deploy: db7520d9fce5a2f11e7f5ee98f41acefc100960d

---
+++ _next/static/5fTgTrA0snDDjXu93b0JQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fblog\u002F[id]","\u002Fprojects\u002Fgame-of-life","\u002Fprojects\u002Fprecise-schedule"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
