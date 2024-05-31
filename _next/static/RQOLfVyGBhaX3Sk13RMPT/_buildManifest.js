de8a3573144f88be41f76b35c2703d624af8e812
joao-arthur <joao-arthur@users.noreply.github.com>
2024-05-31 02:23:02 +0000

    deploy: 7350b390fd7338055242c816951ec4d9e4036661

---
+++ _next/static/RQOLfVyGBhaX3Sk13RMPT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-20bf88aa7fc8d1d0.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
