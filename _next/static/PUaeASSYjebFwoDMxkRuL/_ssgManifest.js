8cb0e2e5048e2adeef332d4914934b52dd1e9c96
joao-arthur <joao-arthur@users.noreply.github.com>
2024-05-31 01:11:41 +0000

    deploy: 4490d5bc4040df97af60e97c331c491dd6da238b

--- _next/static/PUaeASSYjebFwoDMxkRuL/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Fblog\u002F[id]","\u002Fprojects\u002Fgame-of-life","\u002Fprojects\u002Fprecise-schedule"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/blog/post/2023-08-11/page-b5308c1eeada9983.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[465,931,139,700,730,408,360],{2236:function(){}},function(n){n.O(0,[158,660,744],function(){return n(n.s=2236)}),_N_E=n.O()}]);
