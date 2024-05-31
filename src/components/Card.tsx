994797eeed47a33524852d7509dc820ab9c577a5
joao-arthur <joao.lothamer@gmail.com>
2024-05-31 13:01:07 -0300

    feat: engance card style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import type { JSX } from "react";
 import Link from "next/link";
+import { cl } from "../lib/cl";
 
 type Props = {
     readonly title: string;
@@ -11,11 +12,17 @@ export function Card({ title, subTitle, href }: Props): JSX.Element {
     return (
         <div className="flex flex-col w-full max-w-200 p-5 overflow-hidden">
             <Link href={href}>
-                <div className="grow-1 shrink-1 max-w-200 min-w-0 h-40 mx-2 p-5 shadow-md bg-slate-100">
-                    <h1 className="text-2xl text-gray-800">
+                <div className={
+                    cl(
+                        "grow-1 shrink-1 max-w-200 min-w-0 h-40",
+                        "mx-2 p-6",
+                        "shadow-md",
+                        "bg-amber-50 rounded-2xl",
+                        )}>
+                    <h1 className="text-2xl text-amber-900 font-bold">
                         {title}
                     </h1>
-                    <span className="text-gray-600">
+                    <span className="text-amber-800">
                         {subTitle}
                     </span>
                 </div>
