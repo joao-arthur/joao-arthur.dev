bfd865d68f3d3a9fc629bfba15edc3d6d616b457
joao-arthur <joao.lothamer@gmail.com>
2025-02-09 20:50:59 -0300

    feat: languages menu

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -4,7 +4,7 @@ import type { JSX } from "react";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import Link from "next/link";
-import { useColorScheme } from "@mui/material";
+import { Menu, MenuItem, useColorScheme } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -14,7 +14,7 @@ import Drawer from "@mui/material/Drawer";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Menu from "@mui/icons-material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
 import DarkMode from "@mui/icons-material/DarkMode";
 import LightMode from "@mui/icons-material/LightMode";
 import Computer from "@mui/icons-material/Computer";
@@ -23,12 +23,23 @@ import RssFeed from "@mui/icons-material/RssFeed";
 import GitHub from "@mui/icons-material/GitHub";
 import LinkedIn from "@mui/icons-material/LinkedIn";
 import Email from "@mui/icons-material/Email";
+import Translate from "@mui/icons-material/Translate";
 
 export function Header(): JSX.Element {
     const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
     const pathname = usePathname();
     const colorScheme = useColorScheme();
 
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+    
     function openHamburger() {
         setHamburgerIsOpen(true);
     }
@@ -46,77 +57,92 @@ export function Header(): JSX.Element {
     }
 
     return (
-        <AppBar position="static">
-            <Container>
-                <Toolbar>
-                    <IconButton sx={{ mr: 2 }} onClick={openHamburger}>
-                        <Menu />
-                    </IconButton>
-                    <IconButton sx={{ mr: 2 }} onClick={toggleDarkMode}>
-                        {colorScheme?.mode === "dark" ? <DarkMode /> : <LightMode />}
-                    </IconButton>
-                    <Drawer anchor="left" open={hamburgerIsOpen} onClose={closeHamburger}>
-                        <Box sx={{ p: 2 , display: 'flex', flexDirection:"column", height: "100%"}}>
-                            <Box>
-                                <Link href="/" onClick={closeHamburger}>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            <Person />
-                                        </ListItemIcon>
-                                        <ListItemText primary="About" />
-                                    </ListItemButton>
-                                </Link>
-                                <Link href="portfolio" onClick={closeHamburger}>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            <Computer />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Portfolio" />
-                                    </ListItemButton>
-                                </Link>
-                                <Link href="blog" onClick={closeHamburger}>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            <RssFeed />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Blog" />
-                                    </ListItemButton>
-                                </Link>
-                            </Box>
-                            <Box sx={{ flexGrow: 1}}/>
-                            <Box sx={{ display: "flex", columnGap: 2}}>
-                                <a
-                                    href="http://github.com/joao-arthur"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <IconButton>
-                                        <GitHub fontSize="large" />
-                                    </IconButton>
-                                </a>
-                                <a
-                                    href="https://www.linkedin.com/in/joao-lothamer"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <IconButton>
-                                        <LinkedIn fontSize="large" />
-                                    </IconButton>
-                                </a>
-                                <a
-                                    href="mailto://joao.lothamer@gmail.com"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <IconButton>
-                                        <Email fontSize="large" />
-                                    </IconButton>
-                                </a>
-                            </Box>
-                        </Box>
-                    </Drawer>
-                </Toolbar>
-            </Container>
-        </AppBar>
+        <>
+            <AppBar position="static">
+                <Container>
+                    <Toolbar>
+                        <IconButton onClick={openHamburger}>
+                            <MenuIcon />
+                        </IconButton>
+                        <div className="grow"/>
+                        <IconButton onClick={toggleDarkMode}>
+                            {colorScheme?.mode === "dark" ? <DarkMode /> : <LightMode />}
+                        </IconButton>
+                        <IconButton onClick={handleClick} >
+                            <Translate />
+                        </IconButton>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Drawer anchor="left" open={hamburgerIsOpen} onClose={closeHamburger}>
+                <Box sx={{ p: 2, display: "flex", flexDirection: "column", height: "100%" }}>
+                    <Box>
+                        <Link href="/" onClick={closeHamburger}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Person />
+                                </ListItemIcon>
+                                <ListItemText primary="About" />
+                            </ListItemButton>
+                        </Link>
+                        <Link href="portfolio" onClick={closeHamburger}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Computer />
+                                </ListItemIcon>
+                                <ListItemText primary="Portfolio" />
+                            </ListItemButton>
+                        </Link>
+                        <Link href="blog" onClick={closeHamburger}>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <RssFeed />
+                                </ListItemIcon>
+                                <ListItemText primary="Blog" />
+                            </ListItemButton>
+                        </Link>
+                    </Box>
+                    <div className="grow"/>
+                    <Box sx={{ display: "flex", columnGap: 2 }}>
+                        <a
+                            href="http://github.com/joao-arthur"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <IconButton>
+                                <GitHub fontSize="large" />
+                            </IconButton>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/joao-lothamer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <IconButton>
+                                <LinkedIn fontSize="large" />
+                            </IconButton>
+                        </a>
+                        <a
+                            href="mailto://joao.lothamer@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <IconButton>
+                                <Email fontSize="large" />
+                            </IconButton>
+                        </a>
+                    </Box>
+                </Box>
+            </Drawer>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={() => {}}>🇧🇷 Português</MenuItem>
+                <MenuItem onClick={() => {}}>🇦🇷 Castellano</MenuItem>
+                <MenuItem onClick={() => {}}>🇺🇸 English</MenuItem>
+            </Menu>
+        </>
     );
 }
