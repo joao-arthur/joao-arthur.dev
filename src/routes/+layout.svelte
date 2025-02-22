070dfda9a566deb83d78ae04840c3abeeb4c9dbb
joao-arthur <joao.lothamer@gmail.com>
2025-02-22 11:18:15 -0300

    refactor: js to ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Header from "$lib/components/layout/Header.svelte";
     import Footer from "$lib/components/layout/Footer.svelte";
-    import { language, theme } from "$lib/src/store";
+    import { language, theme } from "$lib/src/store.js";
     import { onMount } from "svelte";
     import { page } from "$app/state";
 
